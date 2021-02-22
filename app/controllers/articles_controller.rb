532e6db4dd4b16434ee4585de6b348e6aeba9eb6
haryorthundey <santaayotunde95@gmail.com>
2021-02-21 22:37:56 -1200

    The show action and article listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
 
   def show
-    @article = Article.find(params[:id])
+  @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+
   end
 
 end
